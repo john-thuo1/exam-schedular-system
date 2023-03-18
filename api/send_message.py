7d22685ea80f75daf083c89afc4de09960aba1cb
john-thuo1 <108690517+john-thuo1@users.noreply.github.com>
2023-03-19 01:57:03 +0300

    Modified signals

--- api/send_message.py
+++ api/send_message.py
@@ -13,7 +13,7 @@ def send(phone_number, message="Hi from Africa's Talking"):
         "Accept": "application/json",
         "Content-Type": "application/x-www-form-urlencoded",
         # enter your api key here
-        'apiKey': 'e1afe82a388175d7f467177acb58dd039ba0135541ae1987a4d9d88455d07da3'
+        'apiKey': '7141b829cf6ae244cda1b3c7f7b5f0168912264ff6438f2d5dee3e335d15da03'
     }
 
     req = requests.post('https://api.sandbox.africastalking.com/version1/messaging', data=payload, headers=header)
--- api/signals.py
+++ api/signals.py
@@ -2,9 +2,10 @@ from django.db.models.signals import post_save
 import api.send_message as send_message
 from django.dispatch import receiver
 from api.generate_code import get_code
-
 from users.models import ExamEnrolled
 
+
+global booking_code
 
 @receiver(post_save, sender=ExamEnrolled)
 def student_exam_registration(sender, instance, created, **kwargs):
@@ -14,7 +15,6 @@ def student_exam_registration(sender, instance, created, **kwargs):
         # Get the Registered Exam 
         exam = str(instance.exams)
 
-        global booking_code
         booking_code = get_code()
         # parent = student.objects.prefetch_related('students').all()
         parent = student.parent_set.first()
@@ -32,20 +32,3 @@ def student_exam_registration(sender, instance, created, **kwargs):
         send_message.send(student.phone_number, message_to_student)
         send_message.send(parent.phone_number, message_to_parent)
 
-
-# @receiver(post_save, sender=ExamEnrolled)
-# def student_exam_registration(sender, instance, created, **kwargs):
-#     if created:
-#         # Get the student name
-#         student = instance.student
-#         # Get the Registered Exam 
-#         exam = str(instance.exams)
-#         # parent = student.objects.prefetch_related('students').all()
-
-#         parent = student.parent_set.first()
-#         message = f'Dear Parent,'\
-#                   f'Your Child, {student.get_full_name()} has registered for {exam}.\n'\
-#                   f'The Exam Booking Code is : {booking_code}.'\
-#                   f'Please remind your child to provide this booking code on the day of Exam.\n'\
-#                   f'Failure to which they will not be allowed to the Examination Room.\n'
-#         send_message.send(parent.phone_number, message)
