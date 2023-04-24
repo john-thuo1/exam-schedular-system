d7a704cfbefca8d5ec88f1cd5f2958bde484776b
john-thuo1 <108690517+john-thuo1@users.noreply.github.com>
2023-04-24 17:23:42 +0300

    randomized booking code generation

--- api/generate_code.py
+++ api/generate_code.py
@@ -1,7 +1,7 @@
 import string
 import random
 
-
+# Generates booking codes
 def get_code():
     length_of_code = 6
     value = ''.join(random.choices(string.ascii_uppercase + string.digits, k = length_of_code)) 
