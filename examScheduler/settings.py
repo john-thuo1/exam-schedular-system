6441e678f31626c61f98a3cc6f552e2011b40156
john-thuo1 <108690517+john-thuo1@users.noreply.github.com>
2023-04-24 17:20:40 +0300

    set up ngrok url (temporary url)

--- examScheduler/settings.py
+++ examScheduler/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# set up your own ngrok link 
+# set up your own ngrok url
 ALLOWED_HOSTS = ['127.0.0.1', 'a72d-197-232-140-188.eu.ngrok.io']
 
 
