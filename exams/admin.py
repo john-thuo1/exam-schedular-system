fcee50d8b138789bed741a932f44d337705e134d
john-thuo1 <108690517+john-thuo1@users.noreply.github.com>
2023-04-24 17:25:09 +0300

    registered models to django admin

--- exams/admin.py
+++ exams/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib import admin
 from exams.models import Unit, Course, Exam
 
-# Registering models for our project
+# Registering models to the Admin 
 
 admin.site.register(Unit)
 admin.site.register(Course)
